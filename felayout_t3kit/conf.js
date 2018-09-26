fc5018abcd682fdf1b754cccfbdd39585bff9470
Hannes Leutloff <hannes.leutloff@dkd.de>
2018-09-26 12:35:57 +0200

    [TASK] remove grunt; clean up packages

--- felayout_t3kit/conf.js
+++
@@ -1,8 +0,0 @@
-const conf = module.exports = {}
-
-conf.var = {
-  lessFolder: '../Resources/Public/less',
-  cssFolder: '../Resources/Public/css',
-  components: `node_modules`,
-  proxy: `localhost:8888`
-}
